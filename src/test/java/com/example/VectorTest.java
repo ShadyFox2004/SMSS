a6e60aec3db3958f929823ce34750c8209ae832b
ShadyFox2004 <amboudreau.amb@gmail.com>
2021-11-11 12:30:05 -0500

    Vector

--- src/test/java/com/example/VectorTest.java
+++ src/test/java/com/example/VectorTest.java
@@ -1,15 +1,14 @@
 package com.example;
 
 import static org.junit.Assert.*;
-
 import org.junit.Test;
 
 public class VectorTest {
     @Test
     public void testAdd() {
-        String actual = new Vector(3,3).add(new Vector(3, 3)).toString();
+        String actual = new Vector(3, 3).add(new Vector(3, 3)).toString();
         String expected = new Vector(6, 6).toString();
-        
+
         assertEquals(expected, actual);
     }
 
@@ -17,9 +16,9 @@ public class VectorTest {
     public void testDotProduct() {
         Vector firstVector = new Vector(0.0, 1.0);
         Vector secondVector = new Vector(1.0, 0.0);
-        
+
         double expected = 0.0;
-        double actual = firstVector.dotProduct(secondVector); 
+        double actual = firstVector.dotProduct(secondVector);
         assertEquals(expected, actual, 0.000001);
     }
 
@@ -27,18 +26,18 @@ public class VectorTest {
     public void testGetAngleBetween() {
         Vector firstVector = new Vector(0.0, 1.0);
         Vector secondVector = new Vector(1.0, 0.0);
-        
+
         double expected = Math.PI / 2.0;
-        double actual = Vector.getAngleBetween(firstVector, secondVector); 
+        double actual = Vector.getAngleBetween(firstVector, secondVector);
         assertEquals(expected, actual, 0.000001);
     }
 
     @Test
     public void testGetMagnitude() {
         Vector firstVector = new Vector(2.0, 2.0);
-        
+
         double expected = Math.sqrt(8.0);
-        double actual = firstVector.getMagnitude(); 
+        double actual = firstVector.getMagnitude();
         assertEquals(expected, actual, 0.000001);
     }
 
@@ -47,15 +46,15 @@ public class VectorTest {
         Vector firstVector = new Vector(0.0, 1.0);
 
         String actual = firstVector.rotate(Math.PI / 2.0).toString();
-        String expected = new Vector(-1.0,0.0).toString();
+        String expected = new Vector(-1.0, 0.0).toString();
         assertEquals(expected, actual);
     }
 
     @Test
     public void testScalarProduct() {
-        Vector vector = new Vector(1.0,1.0);
-        
-        String actual =  vector.scalarProduct(3).toString();
+        Vector vector = new Vector(1.0, 1.0);
+
+        String actual = vector.scalarProduct(3).toString();
         String expected = new Vector(3.0, 3.0).toString();
 
         assertEquals(expected, actual);
@@ -65,9 +64,9 @@ public class VectorTest {
     public void testProject() {
         Vector firstVector = new Vector(0.0, 1.0);
         Vector secondVector = new Vector(1.0, 0.0);
-        
+
         String actual = firstVector.projectOn(secondVector).toString();
-        String expected = new Vector(0.0,0.0).toString();
+        String expected = new Vector(0.0, 0.0).toString();
         assertEquals(expected, actual);
     }
 }
