0d5a7f8565e3733e462e0d115c672feb1de9df4e
ShadyFox2004 <amboudreau.amb@gmail.com>
2021-11-15 07:58:30 -0500

    Everything is working !

--- src/main/java/com/example/GameLoop.java
+++ src/main/java/com/example/GameLoop.java
@@ -3,15 +3,15 @@ package com.example;
 public class GameLoop {
     private static final boolean TRUE = true;
     public static void main(String[] args) {
-        Ship myShip = new Ship(new Vector(0, 0),0,0);
-        
-        Module tank = new Module(new Vector(-2,0),2,1);
-        Module cockpit = new Module(new Vector(0,0),0,0);
-        Motor mainThruster = new Motor(new Vector(1,0),1,Math.PI,2);
+        Ship myShip = new Ship(new Vector(0, 0),0,Math.PI/2);
 
-        myShip.addChildren(tank);
+        Module cockpit = new Module(new Vector(0,0),0,0);
+        Motor mainThruster = new Motor(new Vector(0,1),1,0,1);
+        //Motor secondThruster = new Motor(new Vector(0,-2),4,-Math.PI/2,1);
+
         myShip.addChildren(cockpit);
         myShip.addChildren(mainThruster);
+        //myShip.addChildren(secondThruster);
 
         Moment step = myShip.getImpulse();
         System.out.println(step); 
