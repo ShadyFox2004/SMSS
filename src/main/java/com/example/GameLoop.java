0e308af44de9fffb34ecfe042ab728e8b3159ee1
ShadyFox2004 <amboudreau.amb@gmail.com>
2021-11-15 08:35:29 -0500

    Imrpoving game loop

--- src/main/java/com/example/GameLoop.java
+++ src/main/java/com/example/GameLoop.java
@@ -6,7 +6,7 @@ public class GameLoop {
         Ship myShip = new Ship(new Vector(0, 0),0,Math.PI/2);
 
         Module cockpit = new Module(new Vector(0,0),0,0);
-        Motor mainThruster = new Motor(new Vector(0,1),1,0,1);
+        Motor mainThruster = new Motor(new Vector(0,1),1,0);
         //Motor secondThruster = new Motor(new Vector(0,-2),4,-Math.PI/2,1);
 
         myShip.addChildren(cockpit);
--- src/main/java/com/example/Motor.java
+++ src/main/java/com/example/Motor.java
@@ -1,16 +1,21 @@
 package com.example;
 
 public class Motor extends Module {
-    public Motor(Vector position, double mass, double direction, double force) {
+    public Motor(Vector position, double mass, double direction) {
         super(position, mass, direction);
-        this.setForce(force);
-        this.setPower(10);
+        this.setForce(DEFAULT_FORCE);
+        this.setPower(DEFAULT_POWER);
+        this.setConsomation(DEFAULT_CONSOMATION);
         //TODO Auto-generated constructor stub
     }
 
     private double force;
     private double power;
     private double consomation;
+
+    public static final double DEFAULT_FORCE = 0;
+    public static final double DEFAULT_POWER = 0;
+    public static final double DEFAULT_CONSOMATION = 0;
 
     public double getForce() {
         return force;
@@ -46,9 +51,8 @@ public class Motor extends Module {
         // Thrust calculation
         double angle;
         linearImpulse = new Vector(this.getForce()*this.getPower(),0).rotate(this.getDirection());
-        System.out.println("Motor.calculateMomentImpact()");
-        System.out.println(linearImpulse);
 
+        // Rotation calculation 
         Vector normal = this.getPosition().rotate(Math.PI);
         angularImpulse = linearImpulse.projectOn(normal).getMagnitude()*this.getPosition().getMagnitude();
         angle = Vector.getAngleBetween(normal, linearImpulse);
@@ -57,12 +61,6 @@ public class Motor extends Module {
             angularImpulse = -angularImpulse;
         }
 
-        // Torque calculation
-        //angle = Vector.getAngleBetween(this.getPosition(), linearImpulse);
-        //angularImpulse = linearImpulse.getMagnitude()*angle*this.getPosition().getMagnitude();
-        //if (Vector.getAngleBetween(this.getPosition().rotate(Math.PI), linearImpulse) > Math.PI / 2.0){
-        //    angularImpulse = -angularImpulse;
-        //}
         impact = new Moment(linearImpulse, angularImpulse);
         return impact;
     }
